cd7bbf8cc5c41a4df7997bbe98ed68971274a416
VizzWard <vicen102rodr@gmail.com>
2024-07-08 22:18:16 -0600

    added the file ArrayMultidimensionalArray to Array

---
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Arrays/ArreglosMultidimencionales.java
@@ -0,0 +1,12 @@
+package org.vzw.PickALanguage.LearnTheFundamentals.Arrays;
+
+/**
+ * <h1>Definicion y Estructura</h1>
+ *
+ * En su forma más básica, un array multidimensional en Java puede verse como un «array de arrays», donde cada elemento
+ * del array principal es, a su vez, otro array. Esto permite crear estructuras bidimensionales, como matrices, o
+ * incluso estructuras tridimensionales o de mayor dimensión, dependiendo de las necesidades del proyecto.
+ */
+
+public class ArreglosMultidimencionales {
+}
