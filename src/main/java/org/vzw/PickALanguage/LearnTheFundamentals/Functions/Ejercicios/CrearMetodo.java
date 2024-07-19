aa7c7fbabf88e8a0b09643c1f322fa24cdd55b69
VizzWard <vicen102rodr@gmail.com>
2024-07-19 05:28:04 -0600

    CrearMetodo Resuelto

--- src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/CrearMetodo.java
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/CrearMetodo.java
@@ -4,5 +4,11 @@ package org.vzw.PickALanguage.LearnTheFundamentals.Functions.Ejercicios;
  * Crea un método llamado saludar que imprima "Hola, mundo!" en la consola.
  */
 public class CrearMetodo {
-    
+    static void saludar(){
+        System.out.println("Hola Mundo!!!");
+    }
+
+    public static void main(String[] args) {
+        saludar();
+    }
 }
