107e6e91ce0f0f9d86467e7a9c0593093fef212c
VizzWard <vicen102rodr@gmail.com>
2024-07-19 09:09:35 -0600

    EjMethodScope Resuelto

--- src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjMethodScope.java
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjMethodScope.java
@@ -4,4 +4,16 @@ package org.vzw.PickALanguage.LearnTheFundamentals.Functions.Ejercicios;
  * Crea un método incrementar que incremente un contador declarado fuera del método.
  */
 public class EjMethodScope {
+    static int contador = 0; //Variable de alcance a toda la clase
+
+    public static void main(String[] args) {
+        incremento();
+        incremento();
+
+        System.out.println(contador);
+    }
+
+    static void incremento() {
+        contador++;
+    }
 }
