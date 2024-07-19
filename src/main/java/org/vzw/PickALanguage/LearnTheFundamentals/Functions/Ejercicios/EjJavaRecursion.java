2132b241cc68ebacfd1731156540f21eca990007
VizzWard <vicen102rodr@gmail.com>
2024-07-19 09:22:56 -0600

    EjJavaRecursion Resuelto

--- src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjJavaRecursion.java
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjJavaRecursion.java
@@ -4,4 +4,16 @@ package org.vzw.PickALanguage.LearnTheFundamentals.Functions.Ejercicios;
  * Crea un método recursivo llamado factorial que calcule el factorial de un número.
  */
 public class EjJavaRecursion {
+    public static void main(String[] args) {
+        int numFactorial = factorial(15);
+        System.out.println(numFactorial);
+    }
+
+    static int factorial(int n) {
+        if (n > 0){
+            return n + factorial(n -1);
+        } else {
+            return 0;
+        }
+    }
 }
