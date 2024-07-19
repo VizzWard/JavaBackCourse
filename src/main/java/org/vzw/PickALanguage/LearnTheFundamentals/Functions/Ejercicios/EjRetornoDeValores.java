233e93df12ad93445010ec0b4aa144b71ed9e366
VizzWard <vicen102rodr@gmail.com>
2024-07-19 09:00:48 -0600

    EjRetornoDeVariable Resuelto

--- src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjRetornoDeValores.java
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjRetornoDeValores.java
@@ -4,4 +4,15 @@ package org.vzw.PickALanguage.LearnTheFundamentals.Functions.Ejercicios;
  * Crea un método llamado multiplicar que tome dos parámetros a y b y devuelva el producto de ambos.
  */
 public class EjRetornoDeValores {
+    public static void main(String[] args) {
+        int resultado = multiplicar(4, 6);
+        System.out.println(resultado);
+    }
+
+    static int multiplicar(int a, int b) {
+        return a * b;
+    }
+    static float multiplicar(float a, float b) {
+        return a * b;
+    }
 }
