a5744796b7a7230f0a8835c2594f7aa3e6c09b62
VizzWard <vicen102rodr@gmail.com>
2024-07-19 05:45:22 -0600

    EJParametrosYArgumentos Resuelto

--- src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjParametrosYArgumentos.java
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjParametrosYArgumentos.java
@@ -4,4 +4,11 @@ package org.vzw.PickALanguage.LearnTheFundamentals.Functions.Ejercicios;
  * Crea un método llamado imprimirNombre que tome un parámetro nombre y lo imprima en la consola.
  */
 public class EjParametrosYArgumentos {
+    static void imprimirNombre(String nombre) {
+        System.out.println("Nombre: " + nombre);
+    }
+
+    public static void main(String[] args) {
+        imprimirNombre("Vicente");
+    }
 }
