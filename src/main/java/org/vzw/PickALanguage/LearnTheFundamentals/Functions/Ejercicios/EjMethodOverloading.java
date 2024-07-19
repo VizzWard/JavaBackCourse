ed6acb1608fe1fcd542370d8c46fe62f6bbd8335
VizzWard <vicen102rodr@gmail.com>
2024-07-19 09:03:59 -0600

    EjMethodOverloading Resuelto

--- src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjMethodOverloading.java
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Functions/Ejercicios/EjMethodOverloading.java
@@ -5,4 +5,17 @@ package org.vzw.PickALanguage.LearnTheFundamentals.Functions.Ejercicios;
  * parámetros, uno que tome un String y otro que tome un int.
  */
 public class EjMethodOverloading {
+    public static void main(String[] args) {
+        int num = imprimir(3);
+        System.out.println(num);
+
+        String cadena = imprimir("Si");
+        System.out.println(cadena);
+    }
+    static int imprimir(int a){
+        return a + 1;
+    }
+    static String imprimir(String a){
+        return a + ", no";
+    }
 }
