452350a0c5ae52785b29daf9995242af21bb96d9
VizzWard <vicen102rodr@gmail.com>
2024-07-20 23:05:56 -0600

    Updata EjForLoop.java

--- src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Loops/Ejercicios/EjForLoop.java
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/Loops/Ejercicios/EjForLoop.java
@@ -1,4 +1,38 @@
 package org.vzw.PickALanguage.LearnTheFundamentals.Loops.Ejercicios;
 
 public class EjForLoop {
+    //Usar la clase public para ejecutar los metodos de cada ejercicio
+    public static void main(String[] args) {
+
+    }
+
+    static void ejercicio1() {
+        /** <h2>Simple for Loop</h2>
+         * Imprime los números del 1 al 10.
+         */
+    }
+
+    static void ejercicio2() {
+        /** <h2>Nested for Loop</h2>
+         *  Imprime una tabla de multiplicar del 1 al 5.
+         */
+    }
+
+    static void ejercicio3() {
+        /** <h2>For Each Loop</h2>
+         * Imprime todos los elementos de un array de enteros.
+         */
+    }
+
+    static void ejercicio4() {
+        /** <h2>Labeled For Loop</h2>
+         * Usa un labeled loop para salir de dos loops anidados.
+         */
+    }
+
+    static void ejercicio5() {
+        /** <h2>Infinitive for Loop</h2>
+         * Crear un loop infinito que imprima "Hello, World!".
+         */
+    }
 }
