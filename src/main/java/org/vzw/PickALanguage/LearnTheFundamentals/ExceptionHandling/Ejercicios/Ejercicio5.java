5e2f9ae8caafbe437130c4113356c8e221f207f2
VizzWard <vicen102rodr@gmail.com>
2024-07-26 14:21:57 -0600

    Create Ejercicio5

---
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/ExceptionHandling/Ejercicios/Ejercicio5.java
@@ -0,0 +1,39 @@
+package org.vzw.PickALanguage.LearnTheFundamentals.ExceptionHandling.Ejercicios;
+
+
+/**
+ *  Ejercicio 5:
+ *  Crea un programa que valide una entrada de edad.
+ *  Si la edad es menor que cero, lanza una excepción IllegalArgumentException
+ *  con un mensaje adecuado.
+ *
+ *  Instrucciones:
+ * Completa el método validateAge para lanzar una excepción IllegalArgumentException
+ * si la edad es menor que cero.
+ */
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        int age = -5; // Simula una entrada de edad
+
+        try {
+            validateAge(age);
+        } catch (IllegalArgumentException e){
+            System.out.println("Edad incorrecta ingresada ");
+        }
+
+    }
+
+    public static void validateAge(int age) throws IllegalArgumentException {
+        // Lanza una excepción si la edad es inválida
+        if (age < 0) {
+            throw new IllegalArgumentException("La edad introducida debe ser mayor que 0");
+        }
+    }
+}
+
+class IllegalArgumentException extends Exception {
+    public IllegalArgumentException (String str) {
+        // llamando al constructor de la Excepción padre
+        super(str);
+    }
+}
