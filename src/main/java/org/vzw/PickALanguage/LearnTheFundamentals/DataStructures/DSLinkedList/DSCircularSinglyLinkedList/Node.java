4a8c5cfd35591613b8279e93a9a9bcf89b18f95b
VizzWard <vicen102rodr@gmail.com>
2024-08-10 04:45:00 -0600

    Nodo para circular simply list

---
+++ src/main/java/org/vzw/PickALanguage/LearnTheFundamentals/DataStructures/DSLinkedList/DSCircularSinglyLinkedList/Node.java
@@ -0,0 +1,11 @@
+package org.vzw.PickALanguage.LearnTheFundamentals.DataStructures.DSLinkedList.DSCircularSinglyLinkedList;
+
+class Node {
+    Object data;
+    Node next;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+    }
+}
